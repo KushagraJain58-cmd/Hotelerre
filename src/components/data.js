e18877e97d5f0423c744698898a0ecb9332380cf
Kushagra <ishaanjain112@gmail.com>
2023-11-14 16:17:27 +0530

    Produucts page

--- src/components/data.js
+++ src/components/data.js
@@ -12,6 +12,369 @@ import client10 from '../assets/images/Home/Clients/river.jpeg';
 import client11 from '../assets/images/Home/Clients/ananta.jpg';
 import client12 from '../assets/images/Home/Clients/leela.jpg';
 
+import textile1 from '../assets/images/products/kitchen/img1.jpg';
+import textile2 from '../assets/images/products/kitchen/img2.jpg';
+import textile3 from '../assets/images/products/kitchen/img3.jpg';
+import textile4 from '../assets/images/products/kitchen/img4.jpg';
+import textile5 from '../assets/images/products/kitchen/img5.png';
+import textile6 from '../assets/images/products/kitchen/img6.jpg';
+import textile7 from '../assets/images/products/kitchen/img7.jpg';
+import textile8 from '../assets/images/products/kitchen/img8.jpg';
+import textile9 from '../assets/images/products/kitchen/img9.jpg';
+import textile10 from '../assets/images/products/kitchen/img10.jpg';
+import textile11 from '../assets/images/products/kitchen/img11.jpg';
+import textile14 from '../assets/images/products/kitchen/img14.jpg';
+import textile15 from '../assets/images/products/kitchen/img15.jpg';
+import textile18 from '../assets/images/products/kitchen/img18.jpg';
+import textile19 from '../assets/images/products/kitchen/img19.jpg';
+import textile20 from '../assets/images/products/kitchen/img20.jpg';
+import textile21 from '../assets/images/products/kitchen/img21.png';
+import textile22 from '../assets/images/products/kitchen/img22.jpg';
+import textile23 from '../assets/images/products/kitchen/img23.jpg';
+import textile24 from '../assets/images/products/kitchen/img24.jpg';
+import textile25 from '../assets/images/products/kitchen/img25.jpg';
+import textile26 from '../assets/images/products/kitchen/img26.jpg';
+import textile27 from '../assets/images/products/kitchen/img27.jpg';
+import textile28 from '../assets/images/products/kitchen/img28.jpg';
+import textile29 from '../assets/images/products/kitchen/img29.jpg';
+import textile30 from '../assets/images/products/kitchen/img30.jpg';
+import textile31 from '../assets/images/products/kitchen/img31.jpg';
+import textile32 from '../assets/images/products/kitchen/img32.png';
+
+import furniture1 from '../assets/images/products/cooking/img1.png';
+import furniture2 from '../assets/images/products/cooking/img2.jpg';
+import furniture3 from '../assets/images/products/cooking/img3.png';
+import furniture4 from '../assets/images/products/cooking/img4.jpg';
+import furniture5 from '../assets/images/products/cooking/img5.jpg';
+import furniture6 from '../assets/images/products/cooking/img6.jpg';
+import furniture7 from '../assets/images/products/cooking/img7.png';
+
+import decor1 from '../assets/images/products/fnb/img1.webp';
+import decor2 from '../assets/images/products/fnb/img2.webp';
+import decor3 from '../assets/images/products/fnb/img3.jpg';
+import decor4 from '../assets/images/products/fnb/img4.jpg';
+import decor5 from '../assets/images/products/fnb/img5.webp';
+import decor6 from '../assets/images/products/fnb/img6.jpg';
+import decor7 from '../assets/images/products/fnb/img7.png';
+import decor8 from '../assets/images/products/fnb/img8.jpg';
+import decor9 from '../assets/images/products/fnb/img9.jpg';
+import decor10 from '../assets/images/products/fnb/img10.png';
+import decor11 from '../assets/images/products/fnb/img11.png';
+import decor12 from '../assets/images/products/fnb/img12.jpg';
+import decor13 from '../assets/images/products/fnb/img13.jpg';
+
+import improve1 from '../assets/images/products/housekeeping/img1.jpg';
+import improve2 from '../assets/images/products/housekeeping/img2.webp';
+import improve3 from '../assets/images/products/housekeeping/img3.webp';
+import improve4 from '../assets/images/products/housekeeping/img4.jpg';
+import improve5 from '../assets/images/products/housekeeping/img5.jpg';
+import improve6 from '../assets/images/products/housekeeping/img6.jpeg';
+import improve7 from '../assets/images/products/housekeeping/img7.jpg';
+import improve8 from '../assets/images/products/housekeeping/img8.jpg';
+import improve9 from '../assets/images/products/housekeeping/img9.jpeg';
+import improve10 from '../assets/images/products/housekeeping/img10.jpg';
+import improve11 from '../assets/images/products/housekeeping/img11.jpeg';
+import improve12 from '../assets/images/products/housekeeping/img12.jpeg';
+import improve13 from '../assets/images/products/housekeeping/img13.jpeg';
+import improve14 from '../assets/images/products/housekeeping/img14.jpg';
+import improve15 from '../assets/images/products/housekeeping/img15.jpg';
+import improve16 from '../assets/images/products/housekeeping/img16.jpg';
+import improve17 from '../assets/images/products/housekeeping/img17.jpg';
+import improve18 from '../assets/images/products/housekeeping/img18.jpg';
+import improve19 from '../assets/images/products/housekeeping/img19.jpeg';
+import improve20 from '../assets/images/products/housekeeping/img20.jpeg';
+import improve21 from '../assets/images/products/housekeeping/img21.jpg';
+import improve22 from '../assets/images/products/housekeeping/img22.jpg';
+import improve23 from '../assets/images/products/housekeeping/img23.jpeg';
+
+import lineware1 from '../assets/images/products/lineware/img1.png';
+import lineware2 from '../assets/images/products/lineware/img2.jpg';
+import lineware3 from '../assets/images/products/lineware/img3.png';
+import lineware4 from '../assets/images/products/lineware/img4.jpg';
+import lineware5 from '../assets/images/products/lineware/img5.jpg';
+import lineware6 from '../assets/images/products/lineware/img6.jpg';
+import lineware7 from '../assets/images/products/lineware/img7.png';
+
+import paper1 from '../assets/images/products/paper/img1.png';
+import paper2 from '../assets/images/products/paper/img2.jpg';
+import paper3 from '../assets/images/products/paper/img3.png';
+import paper4 from '../assets/images/products/paper/img4.jpg';
+import paper5 from '../assets/images/products/paper/img5.jpg';
+import paper6 from '../assets/images/products/paper/img6.jpg';
+import paper7 from '../assets/images/products/paper/img7.png';
+
+const product_img = [
+	{
+		image: textile1
+	},
+	{
+		image: textile2
+	},
+	{
+		image: textile3
+	},
+	{
+		image: textile4
+	},
+	{
+		image: textile5
+	},
+	{
+		image: textile6
+	},
+	{
+		image: textile7
+	},
+	{
+		image: textile8
+	},
+	{
+		image: textile9
+	},
+	{
+		image: textile10
+	},
+	{
+		image: textile11
+	},
+	{
+		image: textile14
+	},
+	{
+		image: textile15
+	},
+	{
+		image: textile18
+	},
+	{
+		image: textile19
+	},
+	{
+		image: textile20
+	},
+	{
+		image: textile21
+	},
+	{
+		image: textile22
+	},
+	{
+		image: textile23
+	},
+	{
+		image: textile24
+	},
+	{
+		image: textile25
+	},
+	{
+		image: textile26
+	},
+	{
+		image: textile27
+	},
+	{
+		image: textile28
+	},
+	{
+		image: textile29
+	},
+	{
+		image: textile30
+	},
+	{
+		image: textile31
+	},
+	{
+		image: textile32
+	}
+];
+const product_img2 = [
+	{
+		image: furniture1
+	},
+	{
+		image: furniture2
+	},
+	{
+		image: furniture3
+	},
+	{
+		image: furniture4
+	},
+	{
+		image: furniture5
+	},
+	{
+		image: furniture6
+	},
+	{
+		image: furniture7
+	}
+];
+
+const product_img3 = [
+	{
+		image: decor1
+	},
+	{
+		image: decor2
+	},
+	{
+		image: decor3
+	},
+	{
+		image: decor4
+	},
+	{
+		image: decor5
+	},
+	{
+		image: decor6
+	},
+	{
+		image: decor7
+	},
+	{
+		image: decor8
+	},
+	{
+		image: decor9
+	},
+	{
+		image: decor10
+	},
+	{
+		image: decor11
+	},
+	{
+		image: decor12
+	},
+	{
+		image: decor13
+	}
+];
+
+const product_img4 = [
+	{
+		image: improve1
+	},
+	{
+		image: improve2
+	},
+	{
+		image: improve3
+	},
+	{
+		image: improve4
+	},
+	{
+		image: improve5
+	},
+	{
+		image: improve6
+	},
+	{
+		image: improve7
+	},
+	{
+		image: improve8
+	},
+	{
+		image: improve9
+	},
+	{
+		image: improve10
+	},
+	{
+		image: improve11
+	},
+	{
+		image: improve12
+	},
+	{
+		image: improve13
+	},
+	{
+		image: improve14
+	},
+	{
+		image: improve15
+	},
+	{
+		image: improve16
+	},
+	{
+		image: improve17
+	},
+	{
+		image: improve18
+	},
+	{
+		image: improve19
+	},
+	{
+		image: improve20
+	},
+	{
+		image: improve21
+	},
+	{
+		image: improve22
+	},
+	{
+		image: improve23
+	}
+];
+
+const product_img5 = [
+	{
+		image: lineware1
+	},
+	{
+		image: lineware2
+	},
+	{
+		image: lineware3
+	},
+	{
+		image: lineware4
+	},
+	{
+		image: lineware5
+	},
+	{
+		image: lineware6
+	},
+	{
+		image: lineware7
+	}
+];
+
+const product_img6 = [
+	{
+		image: paper1
+	},
+	{
+		image: paper2
+	},
+	{
+		image: paper3
+	},
+	{
+		image: paper4
+	},
+	{
+		image: paper5
+	},
+	{
+		image: paper6
+	},
+	{
+		image: paper7
+	}
+];
+
 const why_data = [
 	{
 		name: 'why',
@@ -112,4 +475,4 @@ const client_data = [
 	}
 ];
 
-export { why_data, client_data };
+export { why_data, client_data, product_img, product_img2, product_img3, product_img4, product_img5, product_img6 };
--- src/pages/ProductsPage/Product.js
+++ src/pages/ProductsPage/Product.js
@@ -1,13 +1,214 @@
 import React, { Fragment } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Cover from '../../components/Cover/Cover';
+import './Product.css';
+import Footer from '../../components/Footer/Footer';
+import product_cover from '../../assets/images/products/products_cover.png';
+import kitchen from '../../assets/images/products/kitchen.jpg';
+import cooking from '../../assets/images/products/cooking.jpg';
+import fnb from '../../assets/images/products/fnb.jpg';
+import housekeeping from '../../assets/images/products/housekeeping.jpg';
+import lineware from '../../assets/images/products/lineware.jpg';
+import paper from '../../assets/images/products/paper.jpg';
+import leaf from '../../assets/images/products/leaf.png';
+import leaf2 from '../../assets/images/products/leaf2.png';
+import leaf3 from '../../assets/images/products/leaf3.png';
+import leaf4 from '../../assets/images/products/leaf4.png';
+import kitchen_img from '../../assets/images/products/kitchen/kitchen_main.jpg';
+import cooking_img from '../../assets/images/products/cooking/cooking_main.jpg';
+import fnb_img from '../../assets/images/products/fnb/fnb_main.jpg';
+import housekeeping_img from '../../assets/images/products/housekeeping/housekeeping_main.jpg';
+import lineware_img from '../../assets/images/products/lineware/lineware_main.jpg';
+import paper_img from '../../assets/images/products/paper/paper_main.jpg';
+import ProductCarousel from '../../components/ProductsCarousel/ProductCarousel';
+import {
+	product_img,
+	product_img2,
+	product_img3,
+	product_img4,
+	product_img5,
+	product_img6
+} from '../../components/data';
 
-const Product = () => {
+const Products = () => {
 	return (
 		<Fragment>
 			<Navbar />
-			<h1>This is products page</h1>
+			<Cover coverImageSrc={product_cover} title="Products" subTitle="We want you to know all about us!" />
+			<section className="product_sec">
+				<div className="product_div">
+					<div className="product_card card1">
+						<img src={kitchen} alt="" />
+						<div className="heading-cover heading1">
+							<h1 className="textile_head">Kitchen Equipments</h1>
+						</div>
+					</div>
+					<img className="leaf" src={leaf} alt="" />
+					<div className="product_card card2">
+						<img src={cooking} alt="" />
+						<div className="heading-cover heading2">
+							<h1 className="furniture_head">Cooking Equipments</h1>
+						</div>
+					</div>
+					<img className="leaf" src={leaf} alt="" />
+					<div className="product_card card3">
+						<img src={fnb} alt="" />
+						<div className="heading-cover heading3">
+							<h1 className="furniture_head">F&B Products</h1>
+						</div>
+					</div>
+				</div>
+				<div className="product_tagline">
+					<img className="leaf2" src={leaf2} alt="" />
+					<h2>One Stop solution to all your hotel needs</h2>
+					<img className="leaf2" src={leaf3} alt="" />
+				</div>
+				<div className="product_div">
+					<div className="product_card card3">
+						<img src={housekeeping} alt="" />
+						<div className="heading-cover heading3">
+							<h1 className="textile_head">Housekeeping</h1>
+						</div>
+					</div>
+					<img className="leaf3" src={leaf4} alt="" />
+					<div className="product_card card2">
+						<img src={lineware} alt="" />
+						<div className="heading-cover heading2">
+							<h1 className="furniture_head">Lineware</h1>
+						</div>
+					</div>
+					<img className="leaf3" src={leaf4} alt="" />
+					<div className="product_card card1">
+						<img src={paper} alt="" />
+						<div className="heading-cover heading1">
+							<h1 className="furniture_head">Paper Napkin</h1>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section className="product_sec sec_back">
+				<div className="prod_container main-img-1">
+					<img src={kitchen_img} alt="" />
+					<div className="prod_content">
+						<h1>Kitchen Equipments</h1>
+						<p>
+							Explore a <b>diverse range</b> of <b>fabrics</b> and <b>home textiles</b> sourced from top
+							manufacturers in India<br />
+							<br />
+							From luxurious silks to durable cottons, we curate quality textiles to{' '}
+							<b>meet your import needs</b>
+							<br />
+							<br />
+							Discover an array of patterns, colours, and textures for your business success.
+						</p>
+					</div>
+				</div>
+				<ProductCarousel images={product_img} />
+			</section>
+			<section className="product_sec sec_back">
+				<div className="prod_container prod_reverse main-img-2">
+					<img src={cooking_img} alt="" />
+					<div className="prod_content content_reverse">
+						<h1>Cooking Equipments</h1>
+						<p className="p_furniture">
+							Browse through a curated selection of exquisite furniture pieces, from{' '}
+							<b>modern to classic designs</b>
+							<br />
+							<br />
+							Our sourcing capabilities include <b>durable and stylish furniture</b> crafted with
+							precision
+							<br />
+							<br />
+							A blend of <b>aesthetic appeal and functionality</b>. Find the perfect pieces to elevate
+							your market presence
+						</p>
+					</div>
+				</div>
+				<ProductCarousel images={product_img2} />
+			</section>
+			<section className="product_sec sec_back">
+				<div className="prod_container main-img-1">
+					<img src={fnb_img} alt="" />
+					<div className="prod_content">
+						<h1>F&B Products</h1>
+						<p>
+							Discover an exclusive collection of{' '}
+							<b>home decoration, accessories, kitchenware, tableware, cookware</b>, and <b>crockery</b>.
+							<br />
+							<br />
+							<b>Functional</b> and <b>decorative products</b> to help your target customers elevate their
+							living spaces and dining experiences - manufactured with care in India
+							<br />
+							<br />
+							Tailored to meet your business’ functional, aesthetic and price-sensitive desires.
+						</p>
+					</div>
+				</div>
+				<ProductCarousel images={product_img3} />
+			</section>
+			<section className="product_sec sec_back">
+				<div className="prod_container prod_reverse main-img-2">
+					<img src={housekeeping_img} alt="" />
+					<div className="prod_content content_reverse">
+						<h1>Housekeeping</h1>
+						<p className="p_furniture">
+							Discover a vast array of high-quality{' '}
+							<b>floor tiles, durable hardware, sleek sanitary ware</b>, and <b>elegant bath fittings</b>
+							<br />
+							<br />
+							Tailored to your business needs, succeed with products that seamlessly blend{' '}
+							<b>functionality, style</b>, and <b>durability</b>
+							<br />
+							<br />
+							Choose from a curated range of designs to suit your aesthetic preferences.
+						</p>
+					</div>
+				</div>
+				<ProductCarousel images={product_img4} />
+			</section>
+			<section className="product_sec sec_back">
+				<div className="prod_container main-img-1">
+					<img src={lineware_img} alt="" />
+					<div className="prod_content">
+						<h1>Lineware</h1>
+						<p>
+							Discover an exclusive collection of{' '}
+							<b>home decoration, accessories, kitchenware, tableware, cookware</b>, and <b>crockery</b>.
+							<br />
+							<br />
+							<b>Functional</b> and <b>decorative products</b> to help your target customers elevate their
+							living spaces and dining experiences - manufactured with care in India
+							<br />
+							<br />
+							Tailored to meet your business’ functional, aesthetic and price-sensitive desires.
+						</p>
+					</div>
+				</div>
+				<ProductCarousel images={product_img5} />
+			</section>
+			<section className="product_sec sec_back">
+				<div className="prod_container prod_reverse main-img-2">
+					<img src={paper_img} alt="" />
+					<div className="prod_content content_reverse">
+						<h1>Paper Napkins and Disposables</h1>
+						<p className="p_furniture">
+							Discover a vast array of high-quality{' '}
+							<b>floor tiles, durable hardware, sleek sanitary ware</b>, and <b>elegant bath fittings</b>
+							<br />
+							<br />
+							Tailored to your business needs, succeed with products that seamlessly blend{' '}
+							<b>functionality, style</b>, and <b>durability</b>
+							<br />
+							<br />
+							Choose from a curated range of designs to suit your aesthetic preferences.
+						</p>
+					</div>
+				</div>
+				<ProductCarousel images={product_img6} />
+			</section>
+			<Footer />
 		</Fragment>
 	);
 };
 
-export default Product;
+export default Products;
